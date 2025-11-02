8fbf47015f4486439ca8da799d58c124a9046907
rayllansilva <rayllansilva2011@gmail.com>
2025-11-02 14:44:29 -0400

    notas-atletas.js

--- notas-atletas.js
+++ notas-atletas.js
@@ -1,1 +1,45 @@
+let atletas = [
+  {
+    nome: "Cesar Abascal",
+    notas: [10, 9.34, 8.42, 10, 7.88]
+  },
+  {
+    nome: "Fernando Puntel",
+    notas: [8, 10, 10, 7, 9.33]
+  },
+  {
+    nome: "Daiane Jelinsky",
+    notas: [7, 10, 9.5, 9.5, 8]
+  },
+  {
+    nome: "Bruno Castro",
+    notas: [10, 10, 10, 9, 9.5]
+  }
+];
 
+function calcularMedia(atletas) {
+  for (let i = 0; i < atletas.length; i++) {
+    let notas = atletas[i].notas;
+    let nome = atletas[i].nome;
+
+    // Ordena as notas
+    let notasOrdenadas = notas.slice().sort((a, b) => a - b);
+
+    // Remove a menor e a maior nota
+    let notasValidas = notasOrdenadas.slice(1, 4);
+
+    // Calcula a média das notas válidas
+    let soma = 0;
+    notasValidas.forEach(nota => {
+      soma += nota;
+    });
+    let media = soma / notasValidas.length;
+
+    // Exibe os resultados
+    console.log(`Atleta: ${nome}`);
+    console.log(`Notas Obtidas: ${notas.join(",")}`);
+    console.log(`Média Válida: ${media}\n`);
+  }
+}
+
+calcularMedia(atletas);
